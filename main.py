ecccdbce79995ed8c36cc14d2e2ad87149a11f92
jreiss <jreiss@xes-inc.com>
2024-06-18 15:20:46 -0500

    Clean syntax

--- main.py
+++ main.py
@@ -3,40 +3,37 @@ import numpy as np
 from time import time
 from Capture import GameCapture
 
-def game_init():
-    GAME_WINDOW = 'T-Rex Game – Google Dino Run - Google Chrome'
+GAME_WINDOW = 'T-Rex Game – Google Dino Run - Google Chrome'
 
+def render_loop():
     gamecap = GameCapture(GAME_WINDOW)
-    print("[game_init] initialized capture settings")
-    
-    return gamecap
-
-def game_loop(gamecap):
+    print("[render_loop] initialized capture settings")
 
     # OpenCV window to display the video
-    print("[game_loop] starting game stream")
+    print("[render_loop] starting game stream")
 
     while True:
+        # Capture starting time
         start_time = time()
 
         img = gamecap.take_screenshot()
 
         cv2.imshow("Game Stream", img)
 
-        elapsed_time = time() - start_time
-        print(f"FPS: {1 / elapsed_time}")
+        #elapsed_time = time() - start_time
+        #print(f"FPS: {1 / elapsed_time}")
 
         # Exit the loop if 'q' is pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            print("[game_loop] game streaming stopped")
+            print("[render_loop] game streaming stopped")
             cv2.destroyAllWindows()
             break
 
-    print("[game_loop] exiting game stream")
+    # Exiting render stream
+    print("[render_loop] exiting game stream")
 
 def main():
-    gamecap = game_init()
-    game_loop(gamecap)
+    render_loop()
 
 if __name__ == '__main__':
     main()
